6f6e916e3610e38e54f498b6503b123aadcad3e6
estherdellacagna <86942596+estherdellacagna@users.noreply.github.com>
2022-08-04 16:58:46 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://comfy-crepe-8a992d.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Esther Della Cagna
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/estherdellacagna/new-react-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced.
-          </a>{" "}
-        </p>
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://comfy-crepe-8a992d.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Esther Della Cagna
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/estherdellacagna/new-react-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced.
+            </a>{" "}
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
