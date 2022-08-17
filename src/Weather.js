de611af2c4d120b8a8ca3ea86b8c53d5d92c3d2f
estherdellacagna <86942596+estherdellacagna@users.noreply.github.com>
2022-08-17 12:04:46 +0200

    incorporated the coordinated from the API

--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
@@ -56,7 +57,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast/>
+        <WeatherForecast coordinates={weatherData.coordinates}/>
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,20 +1,32 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
-import "./WeatherForecast.css"
+import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast(){
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          <div className="col">
-            <div className="WeatherForecast-day">Thur</div>
-            <WeatherIcon code="01d" size={36} />
-            <div className="WeatherForecast-temperature">
-              <span className="WeatherForecast-temperature-max">13°C</span>
-              <span className="WeatherForecast-temperature-min">9°C</span>
-            </div>
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+  let apiKey = "695f78b1636cbc08c8cdb60f209cdb67";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thur</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">13°C</span>
+            <span className="WeatherForecast-temperature-min">9°C</span>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
