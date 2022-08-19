83658b8756b12eb07e4dd4a3812bceb54e88cad3
estherdellacagna <86942596+estherdellacagna@users.noreply.github.com>
2022-08-19 11:39:18 +0200

    fixed errors

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,10 +7,9 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     setLoaded(false);
-  },
-  [props.coordinates]);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -21,16 +20,17 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-            {forecast.map(function(dailyForecast,index){
-                if (index <5){
-                return(
-          <div className="col" key={index}>
-            <WeatherForecastDay data={dailyForecast} />
-          </div>
-                );
-                }
-
-            })}
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
